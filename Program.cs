4b7dccf34376f8b2a2ab11bf60190f9229ec1cfc
MATIAS ALBERTO FRAIMAN <49979893@ort>
2026-08-12 09:03:56 -0300

    low

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddSession();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -19,6 +19,8 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapStaticAssets();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
